6d13387b113f0da18291c600466a20da4ad3c305
Joao Antonio Santos <23306883+JoaoPalmasBR@users.noreply.github.com>
2018-03-06 05:03:17 -0300

    atividade-2-crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,22 @@ import {Disciplina} from './disciplina.model';
 })
 export class AppComponent {
   selecionado = null;
+  cod = null;
   nome = null;
   descricao = null;
+  visivel = false;
+  disciplina = null;
+  paraeditar = null;
+  nova = null;
   disciplinas = [
-    new Disciplina('Língua Portuguesa', 'O objetivo norteador da BNCC de ' +
+    new Disciplina(0, "Língua Portuguesa", 'O objetivo norteador da BNCC de ' +
       'Língua Portuguesa é garantir a todos os alunos o acesso aos saberes ' +
       'linguísticos necessários para a participação social e o exercício da ' +
       'cidadania, pois é por meio da língua que o ser ' +
       'humano pensa, comunica-se, tem acesso à informação, expressa e ' +
       'defende pontos de vista, partilha ou constrói visões de mundo e ' +
       'produz conhecimento.'),
-    new Disciplina('Educação Física', 'A Educação Física é o componente ' +
+    new Disciplina(1,'Educação Física', 'A Educação Física é o componente ' +
       'curricular que tematiza as práticas corporais em suas diversas formas ' +
       'de codificação e significação social, entendidas como manifestações ' +
       'das possibilidades expressivas dos sujeitos e patrimônio cultural ' +
@@ -27,7 +32,7 @@ export class AppComponent {
       'um deslocamento espaço-temporal de um segmento corporal ' +
       'ou de um corpo todo. Logo, as práticas corporais são textos culturais ' +
       'passíveis de leitura e produção.'),
-    new Disciplina('Inglês', 'Aprender a língua inglesa propicia a criação ' +
+    new Disciplina(2,'Inglês', 'Aprender a língua inglesa propicia a criação ' +
       'de novas formas de engajamento e participação dos alunos em um mundo ' +
       'social cada vez mais globalizado e plural, em que as fronteiras ' +
       'entre países e interesses pessoais, locais, regionais, nacionais ' +
@@ -39,7 +44,7 @@ export class AppComponent {
       'formativo que inscreve a aprendizagem de inglês em uma perspectiva ' +
       'de educação linguística, consciente e crítica, na qual as dimensões ' +
       'pedagógicas e políticas são intrinsecamente ligadas.'),
-    new Disciplina('Matemática', 'No Ensino Fundamental, essa área, por ' +
+    new Disciplina(3,'Matemática', 'No Ensino Fundamental, essa área, por ' +
       'meio da articulação de seus diversos campos – Aritmética, Álgebra, ' +
       'Geometria, Estatística e Probabilidade – precisa garantir que os ' +
       'alunos relacionem observações empíricas do mundo real a ' +
@@ -53,7 +58,7 @@ export class AppComponent {
       'algumas propriedades e a verificação de conjecturas, a partir ' +
       'de outras, podem ser estimuladas, sobretudo ao final do ' +
       'Ensino Fundamental.'),
-    new Disciplina('Ciências', 'Ao estudar Ciências, as pessoas aprendem ' +
+    new Disciplina(4,'Ciências', 'Ao estudar Ciências, as pessoas aprendem ' +
       'a respeito de si mesmas, da diversidade e dos processos de evolução ' +
       'e manutenção da vida, do mundo material – com os seus recursos ' +
       'naturais, suas transformações e fontes de energia –, do nosso ' +
@@ -63,10 +68,49 @@ export class AppComponent {
       'compreendam, expliquem e intervenham no mundo em que vivem.')
   ];
 
-  salvar() {
-    const d = new Disciplina(this.nome, this.descricao);
-    this.disciplinas.push(d);
+  cod1 = null;
+  nome1 = null;
+  descricao1 = null;
+  ver(disciplina){
+    this.cod1 = disciplina.cod;
+    this.nome1 = disciplina.nome;
+    this.descricao1 = disciplina.descricao;
+  }
+  editar(disciplina) {
+    this.cod = disciplina.cod;
+    this.nome = disciplina.nome;
+    this.descricao = disciplina.descricao;
+    this.nova = disciplina;
+    this.disciplina = disciplina;
+    this.abrirNovo();
+  }
+  deletar(disciplina){
+    const i = this.disciplinas.indexOf(disciplina);
+    this.disciplinas.splice(i, 1);
+  }
+  salvar(disciplina) {
+    if (this.nova) {
+      this.nova.cod = this.cod;
+      this.nova.nome = this.nome;
+      this.nova.descricao = this.descricao;
+    }
+    else {
+      const d = new Disciplina(this.cod, this.nome, this.descricao);
+      this.disciplinas.push(d);
+    }
+    this.cod = null;
+    this.nome = null;
+    this.descricao = null;
+    this.nova = null;
+  }
+  abrirNovo(){
+    this.visivel = true;
+  }
+  cancelar(){
+    this.visivel = false;
+    this.cod = null;
     this.nome = null;
     this.descricao = null;
   }
+
 }
